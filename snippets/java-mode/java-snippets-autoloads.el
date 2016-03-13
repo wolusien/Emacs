63f87e1d1275d619d3a20465ba9f8aaf2190401f
wolusien <djebali.wissam@gmail.com>
2016-03-13 19:14:59 +0100

    Ajout de snippets pour java

---
+++ snippets/java-mode/java-snippets-autoloads.el
@@ -0,0 +1,31 @@
+;;; java-snippets-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (java-snippets-initialize) "java-snippets" "java-snippets.el"
+;;;;;;  (22245 44323 104105 51000))
+;;; Generated autoloads from java-snippets.el
+
+(autoload 'java-snippets-initialize "java-snippets" "\
+
+
+\(fn)" nil nil)
+
+(eval-after-load 'yasnippet '(java-snippets-initialize))
+
+;;;***
+
+;;;### (autoloads nil nil ("java-snippets-pkg.el") (22245 44323 179490
+;;;;;;  991000))
+
+;;;***
+
+(provide 'java-snippets-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; java-snippets-autoloads.el ends here
